42bdc9e02fd96cdee42ee2a4dfaf436c5ada8f22
ghostNote <selcukbilgen@hotmail.com>
2022-03-30 00:17:52 +0300

    understanding publisher and subscriber

--- RabbitMQ/RabbitMQ.Publisher/Program.cs
+++ RabbitMQ/RabbitMQ.Publisher/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+channel.QueueDeclare("hello-queue", true, false, false);
+
+string message = "Hello-RabbitMQ";
+var messageBody = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(String.Empty,"hello-queue",null,messageBody);
+
+Console.WriteLine("Mesaj gönderildi");
+Console.ReadLine();
