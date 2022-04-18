5ee770d7de7cfe3b6da34eba0e87717fe38a731a
ghostNote <selcukbilgen@hotmail.com>
2022-04-19 00:04:26 +0300

    creating mvc project & sql server migrations  implementation

--- RabbitMQWeb.ExcelCreating/Models/AppDbContext.cs
+++ RabbitMQWeb.ExcelCreating/Models/AppDbContext.cs
@@ -1,8 +1,13 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace RabbitMQWeb.ExcelCreating.Models;
 
-public class AppDbContext: IdentityDbContext
+public class AppDbContext : IdentityDbContext
 {
-    
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<UserFile> UserFiles { get; set; }
 }
