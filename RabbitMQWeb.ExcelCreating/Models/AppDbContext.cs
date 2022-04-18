9d1da46b883ebbf98864c67f7a534b6a18848d0c
ghostNote <selcukbilgen@hotmail.com>
2022-04-18 22:05:43 +0300

    project structure created

---
+++ RabbitMQWeb.ExcelCreating/Models/AppDbContext.cs
@@ -0,0 +1,8 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace RabbitMQWeb.ExcelCreating.Models;
+
+public class AppDbContext: IdentityDbContext
+{
+    
+}
---
+++ RabbitMQWeb.ExcelCreating/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RabbitMQWeb.ExcelCreating.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
