c52bd634cd819df6fa74b2a70d6d2b72a940953d
ghostNote <selcukbilgen@hotmail.com>
2022-04-29 12:19:54 +0300

    word to pdf project created

---
+++ RabbitMQWeb.WordToPdf/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using RabbitMQWeb.WordToPdf.Models;
+
+namespace RabbitMQWeb.WordToPdf.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult WordToPdfPage()
+    {
+        return View();
+    }
+    
+    [HttpPost]
+    public IActionResult WordToPdfPage(WordToPdfModel wordToPdf)
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ RabbitMQWeb.WordToPdf/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace RabbitMQWeb.WordToPdf.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
